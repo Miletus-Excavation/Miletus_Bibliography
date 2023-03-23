57392e0ac8ac86e906af0e76c0d19e7872343af4
Lisa Steinmann <37840007+lsteinmann@users.noreply.github.com>
2023-03-23 19:12:23 +0100

    comments

--- Exporte_PDF/setup_tex_files.R
+++ Exporte_PDF/setup_tex_files.R
@@ -4,27 +4,27 @@ library(stringi)
 #library(utf8)
 
 
-remove_na_cols <<- function(data) {
+remove_na_cols <- function(data) {
   na_cols <- apply(data, function(x) all(is.na(x)), MARGIN = 2)
   data <- data[, !na_cols]
   return(data)
 }
 
-# Bibliographie laden
+# get the bibliography from the csv file
 bib <- read.csv("Exporte/Milet_Bibliography_CSV.csv", encoding = "UTF-8", na.strings = "") %>%
-  remove_na_cols()
-bib$Publication.Year <- as.numeric(bib$Publication.Year)
-bib$Date.Added <- as.Date(bib$Date.Added)
-bib$Date.Modified <- as.Date(bib$Date.Modified)
+  remove_na_cols() %>%
+  type.convert(as.is = TRUE)
 
-
-# Exportieren von defbibcheck und sections nach Jahren
+# check if Publication.Year makes sense
 min(bib$Publication.Year, na.rm = TRUE)
 max(bib$Publication.Year, na.rm = TRUE)
 
+# a table of years
 years_tbl <- table(bib$Publication.Year)
 years <- as.numeric(names(years_tbl))
 
+# a bit of a work-around... define a bibcheck for each year that we can use
+# later to tell biblatex which entries to use
 defbibcheck <- "NA"
 for (y in length(years):1) {
   defbibcheck <- c(defbibcheck, paste("\\defbibcheck{yr", years[y], "}{%
@@ -39,23 +39,28 @@ defbibcheck <- defbibcheck[-1]
 write(defbibcheck, file = "Exporte_PDF/defbibcheck_by_year.tex")
 
 
-
+# for each year...
 bibsections <- "NA"
 for (y in length(years):1) {
+  # we produce a section for latex
   a <- paste("\\section*{", years[y], "}", sep = "")
+  # add it to the table of contents with a custom name
   a <- paste(a, "\n", "\\addcontentsline{toc}{section}{", years[y], "}%", sep = "")
+  # have latex print the bibliography according to the corresponding bibcheck
   a <- paste(a, "\n", "\\printbibliography[check=yr", years[y], ",heading=none, env=compactbib]", sep = "")
+  # bind
   bibsections <- c(bibsections, a)
 }
+# remove NA
 bibsections <- bibsections[-1]
 
-
+# save the file
 write(bibsections, file = "Exporte_PDF/bibsections_by_year.tex")
 
 
 
-# Exportieren von defbibcheck und sections nach Autoren #TODO
-
+# Exportieren von defbibcheck und sections nach Autoren
+# get all authors
 authors <- bib$Author
 authors <- authors %>%
   strsplit("; ") %>%
@@ -63,46 +68,75 @@ authors <- authors %>%
   unique() %>%
   sort()
 
-
+# get a list of first letters and replace cyrillic and greek with their
+# latin representation
 letters <- toupper(substr(authors, 1, 1))
 letters <- stri_trans_general(letters, "cyrillic-latin/bgn")
 letters <- stri_trans_general(letters, "Greek-Latin/BGN")
 
+# associate it with each author
 names(authors) <- letters
 
+# sort the unique vector of authors 
 letters <- sort(unique(names(authors)))
 
+# check it out
 authors
 
+# the citation key is saved in "extra" with the prefix Citation Key: 
+# so we split the string along that
 key <- strsplit(bib$Extra, "Citation Key: ")
-
+# and get the second element, which should be the latex-key
 key <- unlist(lapply(key, function(x) x[[2]]))
+# it is kind of important that there is nothing else saved in the
+# extra field... be sure to check that sometimes and fix the database
+# accordingly
 
 #key[which(!grepl("_", key))]
 
+#
 key <- strsplit(key, " ")
 #key
 key <- unlist(lapply(key, function(x) x[[1]]))
 
+# save the result as the tex_key
 bib$tex_key <- key
 
-letters
+## Check for wrong entries and possible errors
+which(table(bib$tex_key) > 1)
 
-#library(stringi)
-#stri_trans_list()
+# regex of how the keys should look according to my settings
+texkey_regex <- "^[a-z-]+_[[:alnum:]]+_(\\d{4}[a-z]{0,1}|o\\.J\\.)$"
+texkey_false <- which(!grepl(texkey_regex, bib$tex_key))
+
+# see if there are any wrong keys
+View(bib[texkey_false,c("Author", "Publication.Year", "Title", "tex_key")])
+
+# save them to check out / match in regex editor?
+writeLines(bib[texkey_false,"tex_key"], con = "wrong_tex.txt")
 
 bibstructure <- "NA"
+# for each letter in out alphabet
 for(letter in letters) {
+  # we get all the author names that start with this letter
   subset_authors <- authors[names(authors) == letter]
+  # and produce a section for the letter
   bibstructure <- c(bibstructure, paste("\\section{", letter, "}\n", sep = ""))
+  # for each of the authors that should be in this section
   for (i in 1:length(subset_authors)) {
+    # we select the bibliography accordingly and sort it by 
+    # year of publication, then title
     bib_select <- bib[grepl(subset_authors[i], bib$Author), ]
     bib_select <- bib_select %>% arrange(Publication.Year, Title)
+    # and get the keys with are now in that order
     singleauthkeys <- bib_select$tex_key
+    # we also record the number of publications of this author
     numberofpubs <- length(singleauthkeys)
+    # and build the latex-lines for citing all of those keys
     singleauthkeys <- paste("\\fullcite{", singleauthkeys, "}", sep = "", collapse = "\n\n")
-    #author_fix <- stri_trans_general(authors[i], "cyrillic-latin/bgn")
+    # then we save the author name itself
     author_fix <- subset_authors[i]
+    # and paste our subsection markup including the number of publications
     sectionhead <- paste("\\subsection[", 
                          author_fix, 
                          " (", 
@@ -111,26 +145,19 @@ for(letter in letters) {
                          author_fix, 
                          "}\n", 
                          sep = "")
+    # and all of that gets bound together in one big vector
     bibstructure <- c(bibstructure, paste(sectionhead, singleauthkeys, "\n", sep = ""))
   }
 }
+# remove the NA
 bibstructure <- bibstructure[-1]
 
+# of course, this is inefficient as it grows the vector - but whatever, 
+# we don't do it every day, it doesn't take horribly long, if the bibliography
+# ever counts more than 10k entries I would obviously want to rewrite, but
+# I don't care for now. 
 
-
+# save that so latex can have it
 writeLines(bibstructure, 
            "Exporte_PDF/bibstructure_by_author.tex", 
            useBytes = TRUE)
-
-
-
-## Check for wrong entries
-texkey_regex <- "^[a-z-]+_[[:alnum:]]+_(\\d{4}[a-z]{0,1}|o\\.J\\.)$"
-
-texkey_false <- which(!grepl(texkey_regex, bib$tex_key))
-
-View(bib[texkey_false,c("Author", "Publication.Year", "Title", "tex_key")])
-
-writeLines(bib[texkey_false,"tex_key"], con = "wrong_tex.txt")
-
-which(table(bib$tex_key) > 1)
