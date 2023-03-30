c76a1f6271e444e89b0c12038cf5d34862131dc3
Lisa Steinmann <37840007+lsteinmann@users.noreply.github.com>
2023-03-30 12:43:38 +0200

    test

--- build-bib.R
+++ build-bib.R
@@ -1,9 +1,9 @@
 # build-bib.R
-packages <- c("ggplot", "dplyr", "stringi", "knitr")
+packages <- c("dplyr", "stringi", "knitr")
 
-install.packages(packages, repos = "https://cloud.r-project.org")
+install.packages(packages, repos = "https://cloud.r-project.org", )
 
 source("Exporte_PDF/setup_tex_files.R")
-source("Bibliographie.R")
+#source("Bibliographie.R")
 
 knitr::knit2html("test.Rmd")
