fca6d28251bd2cedcb2091829027b76dacc25533
Lisa Steinmann <37840007+lsteinmann@users.noreply.github.com>
2023-03-30 12:24:58 +0200

    test

--- build-bib.R
+++ build-bib.R
@@ -1,5 +1,10 @@
 # build-bib.R
 
+install.packages("ggplot2", repos = "https://cloud.r-project.org")
+install.packages("dplyr", repos = "https://cloud.r-project.org")
+install.packages("stringi", repos = "https://cloud.r-project.org")
+install.packages("reshape2", repos = "https://cloud.r-project.org")
+
 source("Exporte_PDF/setup_tex_files.R")
 source("Bibliographie.R")
 
