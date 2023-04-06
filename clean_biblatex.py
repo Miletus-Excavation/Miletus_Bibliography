cc3621af130746d0ab540c95cc4026724b421077
Lisa Steinmann <37840007+lsteinmann@users.noreply.github.com>
2023-04-06 19:16:45 +0200

    add bibtexparser to python workflow

---
+++ clean_biblatex.py
@@ -0,0 +1,19 @@
+import bibtexparser
+from bibtexparser.bparser import BibTexParser
+from bibtexparser.bwriter import BibTexWriter
+
+with open('data/Milet_Bibliography_BibLaTeX.bib', encoding="utf8") as bibtex_file:
+    parser = BibTexParser()
+    parser.encoding = "utf-8"
+    parser.ignore_nonstandard_types = False
+    parser.homogenize_fields = True
+    parser.common_strings = False
+    bib_database = bibtexparser.load(bibtex_file, parser=parser)
+
+
+print(bib_database.entries)
+
+writer = BibTexWriter()
+writer.order_entries_by = None #('author', 'year')
+with open('data/Milet_Bibliography_BibLaTeX.bib', 'w', encoding='utf-8') as bibfile:
+    bibfile.write(writer.write(bib_database))
---
+++ clean_bibtex.py
@@ -0,0 +1,22 @@
+import bibtexparser
+from bibtexparser.bparser import BibTexParser
+from bibtexparser.bwriter import BibTexWriter
+from bibtexparser.customization import homogenize_latex_encoding, convert_to_unicode
+
+with open('data/Milet_Bibliography_BibTeX.bib', encoding="utf8") as bibtex_file:
+    parser = BibTexParser()
+    parser.encoding = "utf-8"
+    parser.ignore_nonstandard_types = False
+    parser.homogenize_fields = False
+    parser.common_strings = False
+    parser.customization = homogenize_latex_encoding
+    bib_database = bibtexparser.load(bibtex_file, parser=parser)
+
+
+print(bib_database.entries)
+
+writer = BibTexWriter()
+writer.order_entries_by=None
+writer.display_order
+with open('data/Milet_Bibliography_BibTeX.bib', 'w', encoding='utf-8') as bibfile:
+    bibfile.write(writer.write(bib_database))
